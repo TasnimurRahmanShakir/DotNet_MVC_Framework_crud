1c92566bd5f1f50d6ea25999b15b281201b019ab
TasnimurRahmanShakir <tasnim.bd.cse@gmail.com>
2025-11-18 13:07:28 +0600

    add search option

--- TodoApplicationMVC/Controllers/HomeController.cs
+++ TodoApplicationMVC/Controllers/HomeController.cs
@@ -18,31 +18,52 @@ namespace TodoApplicationMVC.Controllers
 
         public HomeController()
         {
-
             conStr = ConfigurationManager.ConnectionStrings["TodoApplication"].ConnectionString;
         }
-        public ActionResult Index()
-        {
 
-            var query = "select * from todo";
-            List<Todo> todos = getDataList(query);
+        // --- MODIFIED: Index Action ---
+        // Now accepts a 'name' parameter from your search form
+        public ActionResult Index(string name)
+        {
+            string query;
+            SqlParameter param = null;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                // --- SEARCH PATH: Build a LIKE query ---
+                query = "select * from todo WHERE Title LIKE @search OR Description LIKE @search";
+                param = new SqlParameter("@search", $"%{name}%"); // Add wildcards
+                ViewBag.Title = $"Search results for '{name}'";
+            }
+            else
+            {
+                // --- DEFAULT PATH: Get all todos ---
+                query = "select * from todo";
+                ViewBag.Title = "Home Page";
+            }
+
+            List<Todo> todos = getDataList(query, param);
             return View(todos);
         }
 
-
         public ActionResult addTodo()
         {
-
             return View();
         }
 
+        // --- MODIFIED: addTodo Post Action ---
+        // Added Guid generation to ensure the ID is saved
         [HttpPost]
         public ActionResult addTodo(string title, string desc)
         {
-            var query = "insert into todo (Title, Description) values (@title, @desc)";
+            // Create the ID in C#
+            var newId = Guid.NewGuid();
+
+            var query = "insert into todo (Id, Title, Description) values (@id, @title, @desc)";
             using (SqlConnection con = new SqlConnection(conStr))
             {
                 SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", newId); // Add the new Guid
                 cmd.Parameters.AddWithValue("@title", title);
                 cmd.Parameters.AddWithValue("@desc", desc);
 
@@ -51,10 +72,8 @@ namespace TodoApplicationMVC.Controllers
                 con.Close();
             }
 
-            return Redirect("Index");
+            return RedirectToAction("Index"); // Corrected Redirect
         }
-
-
 
         public ActionResult updateTodo(Guid id)
         {
@@ -88,7 +107,7 @@ namespace TodoApplicationMVC.Controllers
         {
             if (id == null)
             {
-                return Redirect("Index");
+                return RedirectToAction("Index");
             }
             var query = "update todo set Title = @title, Description = @desc where Id = @id";
             using (SqlConnection con = new SqlConnection(conStr))
@@ -104,9 +123,6 @@ namespace TodoApplicationMVC.Controllers
             }
             return RedirectToAction("Index");
         }
-
-
-
 
         [HttpGet]
         public ActionResult delete(Guid id)
@@ -124,14 +140,21 @@ namespace TodoApplicationMVC.Controllers
             return RedirectToAction("Index");
         }
 
-
-        private List<Todo> getDataList(string query)
+        // --- MODIFIED: getDataList Method ---
+        // Now accepts an optional SqlParameter
+        private List<Todo> getDataList(string query, SqlParameter parameter = null)
         {
             List<Todo> todos = new List<Todo>();
             using (SqlConnection con = new SqlConnection(conStr))
             {
                 using (SqlCommand cmd = new SqlCommand(query, con))
                 {
+                    // Add the parameter if it exists
+                    if (parameter != null)
+                    {
+                        cmd.Parameters.Add(parameter);
+                    }
+
                     con.Open();
                     SqlDataReader data = cmd.ExecuteReader();
 
@@ -145,6 +168,7 @@ namespace TodoApplicationMVC.Controllers
             }
             return todos;
         }
+
         private Todo MapDataToItem(SqlDataReader data)
         {
             return new Todo
